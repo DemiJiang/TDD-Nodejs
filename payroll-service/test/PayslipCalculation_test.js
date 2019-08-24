389eba77c1627e7347ae92c83f540d4ef18c5ed6
demi.jiang <demi.jiang@myob.com>
2019-08-24 13:32:27 +1000

    add the test change

--- payroll-service/test/PayslipCalculation_test.js
+++ payroll-service/test/PayslipCalculation_test.js
@@ -1,24 +1,24 @@
 const assert = require('assert');
-const payslipCalculate = require('../lib/CSVProcessor');
+const payslipCalculation = require('../lib/PayslipCaliculation');
 
 describe('Calculate Income Tax', function () {
-    it('Tax of annual salary 60050 should equal 922', function () {
-        assert.equal(payslipCalculate.incomeTaxCal(60050), 922)
+    it('Income tax of annual salary 60050 should equal 922', function () {
+        assert.equal(payslipCalculation.incomeTaxCal(60050), 922)
     });
 
-    it('Tax of annual salary 120000 should equal 2669', function () {
-        assert.equal(payslipCalculate.incomeTaxCal(120000), 2669)
+    it('Income tax of annual salary 120000 should equal 2669', function () {
+        assert.equal(payslipCalculation.incomeTaxCal(120000), 2669)
     });
 
 });
 
 describe('Calculate Gross Income', function () {
-    it('Tax of annual salary 60050 should equal 922', function () {
-        assert.equal(payslipCalculate.grossIncomeCal(60050), 5004)
+    it('Gross income of annual salary 60050 should equal 922', function () {
+        assert.equal(payslipCalculation.grossIncomeCal(60050), 5004)
     });
 
-    it('Tax of annual salary 120000 should equal 2669', function () {
-        assert.equal(payslipCalculate.grossIncomeCal(120000), 10000)
+    it('Gross income of annual salary 120000 should equal 2669', function () {
+        assert.equal(payslipCalculation.grossIncomeCal(120000), 10000)
     });
 
 });
