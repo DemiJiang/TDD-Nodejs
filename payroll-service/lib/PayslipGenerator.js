3abacd60a05f4a746acd555594f2929b01777e45
demi.jiang <demi.jiang@myob.com>
2019-08-26 14:00:38 +1000

    make changes

--- payroll-service/lib/PayslipGenerator.js
+++ payroll-service/lib/PayslipGenerator.js
@@ -1,8 +1,8 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-const payslipCalculation = require('../lib/PayslipCaliculation');
-const employeeDatas = [];
+const payslipCalculation = require('./PayslipCaliculator');
+const employees = [];
 const employeePayslips = [];
 const csvWriter = createCsvWriter({
     path: '../output_csv/employee_payslip.csv',
@@ -18,19 +18,20 @@ const csvWriter = createCsvWriter({
 
 fs.createReadStream('../input_csv/employee_data.csv')
     .pipe(csv())
-    .on('data', (data) => employeeDatas.push(data))
+    .on('data', (data) => employees.push(data))
     .on('end', () => {
-        employeeDatas.forEach((employeeData) => {
+        employees.forEach((employee) => {
             const employeePayslip = {};
-            employeePayslip.name = employeeData.firstName + ' ' + employeeData.lastName;
-            employeePayslip.payPeriod = employeeData.paymentStartDate;
-            employeePayslip.grossIncome = payslipCalculation.grossIncomeCal(employeeData.annualSalary);
-            employeePayslip.incomeTax = payslipCalculation.incomeTaxCal(employeeData.annualSalary);
+            employeePayslip.name = employee.firstName + ' ' + employee.lastName;
+            employeePayslip.payPeriod = employee.paymentStartDate;
+            employeePayslip.grossIncome = payslipCalculation.grossIncomeCal(employee.annualSalary);
+            employeePayslip.incomeTax = payslipCalculation.incomeTaxCal(employee.annualSalary);
             employeePayslip.netIncome = employeePayslip.grossIncome - employeePayslip.incomeTax;
-            employeePayslip.super = payslipCalculation.superCal(employeePayslip.grossIncome, employeeData.superRate);
+            employeePayslip.super = payslipCalculation.superCal(employeePayslip.grossIncome, employee.superRate);
             employeePayslips.push(employeePayslip);
         });
         csvWriter
             .writeRecords(employeePayslips)
             .then(()=> console.log('The employee payslip was generated successfully'));
     });
+
